f37c075521d560fb0f9bc6a625754ccfaf6ffe26
Elliot DeNolf <denolfe@gmail.com>
2023-08-31 15:36:48 -0400

    chore: add missing global export

---
+++ packages/payload/src/exports/components/views/Global.ts
@@ -0,0 +1,4 @@
+import { default as DefaultGlobalView } from '../../../admin/components/views/Global/index.js'
+
+export default DefaultGlobalView
+export type { Props } from '../../../admin/components/views/Global/types.js'
