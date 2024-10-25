9dccd99acba8a7aa313a2388daaaceaa1719c8ea
Elliot DeNolf <denolfe@gmail.com>
2024-10-25 11:33:10 -0400

    chore: add writeDefaultAdapter flag, jest args were interfering

--- test/generateDatabaseAdapter.ts
+++ test/generateDatabaseAdapter.ts
@@ -70,6 +70,7 @@ export const allDatabaseAdapters = {
 export function generateDatabaseAdapter(dbAdapter) {
   const databaseAdapter = allDatabaseAdapters[dbAdapter]
   if (!databaseAdapter) {
+    console.log({ dbAdapter })
     throw new Error(`Unknown database adapter: ${dbAdapter}`)
   }
   fs.writeFileSync(
@@ -85,8 +86,10 @@ export function generateDatabaseAdapter(dbAdapter) {
   return databaseAdapter
 }
 
-// Run if called directly, ESM-style. Used in prebuild for test suite build.
+// Run if called directly. Used in test suite build: pnpm build:tests
 if (import.meta.url === `file://${filename}`) {
-  const dbAdapter = process.argv[2]
-  generateDatabaseAdapter(dbAdapter)
+  const writeDefaultAdapter = process.argv.find((arg) => arg === '--writeDefaultAdapter')
+  if (writeDefaultAdapter) {
+    generateDatabaseAdapter('mongodb')
+  }
 }
